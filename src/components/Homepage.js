0d969867ffb169ed5963d38ce40d4ea71d89398c
Zakeria Ali <zak.fined@gmail.com>
2018-12-05 14:46:02 +0000

    Add design and media queries to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,12 +5,19 @@ class Homepage extends Component {
   render() {
     return (
       <div className="homepage">
-        <div className="homepage__welcome">
-          <h1>Organize your tasks with simple todos</h1>
-          <p>No more fancy and hard to navigate UI's</p>
-        </div>
-        <div className="homepage__register">
-          <button>Get started</button>
+        <div className="container">
+          <div className="homepage__welcome">
+            <h1>
+              Organize your tasks with simple <span>todos</span>
+            </h1>
+            <p>
+              No more fancy and hard to navigate UI's.{" "}
+              <span>Just todo it.</span>
+            </p>
+          </div>
+          <div className="homepage__register">
+            <button>Get started</button>
+          </div>
         </div>
       </div>
     );

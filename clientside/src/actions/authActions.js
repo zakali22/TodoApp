276e6f58d14f2e01c97443a066106d7b75103d37
Zakeria Ali <zak.fined@gmail.com>
2018-12-06 22:05:04 +0000

    Fix the proxy issue

--- clientside/src/actions/authActions.js
+++ clientside/src/actions/authActions.js
@@ -2,8 +2,8 @@ import { AUTH_USER } from "./types";
 import axios from "axios";
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get("http://localhost:5000/auth/current_user");
-  console.log(res);
+  const res = await axios.get("/auth/current_user");
+  console.log(res.data);
   dispatch({
     type: AUTH_USER,
     payload: res.data

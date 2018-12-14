59413e395e14fe5dc4c38b5412c211e78d3a9bd5
Zakeria Ali <zak.fined@gmail.com>
2018-12-14 12:16:54 +0000

    Adjust for Heroku deployment

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  facebookKey: process.env.FACEBOOK_CLIENT_ID,
-  facebookSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   mongo_uri: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };

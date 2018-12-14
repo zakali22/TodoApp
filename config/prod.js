13ed56bfe852771658798a8fc751592429a42bfb
Zakeria Ali <zak.fined@gmail.com>
2018-12-14 11:17:09 +0000

    Add prod keys

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  facebookKey: process.env.facebookKey,
+  facebookSecret: process.env.facebookSecret,
+  mongo_uri: process.env.mongo_uri,
+  cookieKey: process.env.cookieKey
+};

7c57d02439a8d2f68a22a135f75e1d940bafccd4
Zakeria Ali <zak.fined@gmail.com>
2018-12-17 14:07:09 +0000

    Remove redux-persist

--- clientside/src/Root.js
+++ clientside/src/Root.js
@@ -4,25 +4,15 @@ import "./index.scss";
 import App from "./components/App";
 
 import { createStore, applyMiddleware, compose } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 const initialState = {};
 const middleware = [thunk];
 
-const persistConfig = {
-  key: "root",
-  storage
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 let store = createStore(
-  persistedReducer,
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
@@ -30,14 +20,6 @@ let store = createStore(
   )
 );
 
-let persistor = persistStore(store);
-
 export default props => {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {props.children}
-      </PersistGate>
-    </Provider>
-  );
+  return <Provider store={store}>{props.children}</Provider>;
 };
